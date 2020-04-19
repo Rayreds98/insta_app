25bdf943e30854a624914497ffec8b53580aa303
Rayreds98 <rayreds1830@gmail.com>
2020-04-19 16:56:22 +0900

    add contacts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+
+  get '/about' => 'static_pages#about'
+  get '/help' => 'static_pages#help'
+
+  resources :contacts, only: [:new, :index, :show]
+
+
 end
