8e92bef4b3c6a7c86e6612188f9e99b838414c67
Rayreds98 <rayreds1830@gmail.com>
2020-04-29 01:22:15 +0900

    add active storage

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
   validates :user_id, presence: true
   validates :content, length: {maximum: 2200}
+
+  def thumbnail
+    return self.image.variant(combine_options:{resize:"320x320^",crop:"320x320+0+0",gravity: :center}).processed
+  end
 end
